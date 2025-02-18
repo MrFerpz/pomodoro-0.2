8d508f0b8afe3d620962bde949118996a35731ac
Fred <leferpz@gmail.com>
2025-02-18 11:18:12 -0500

    Added a header with current coin total

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import './App.css'
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import FightPage from './pages/FightPage'
 import AchievementsPage from './pages/AchievementsPage'
 import ShopPage from './pages/ShopPage'
-
+import ProfilePage from './pages/ProfilePage'
+import Header from "./components/Header/Header"
 
 function App() {
 
+  let coins = 0;
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="fight" element={<FightPage/>}/>
-        <Route path="shop" element={<ShopPage/>}/>
-        <Route path="achievements" element={<AchievementsPage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Header coins={coins}/>
+      <section>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="fight" element={<FightPage/>}/>
+          <Route path="shop" element={<ShopPage/>}/>
+          <Route path="achievements" element={<AchievementsPage/>}/>
+          <Route path="profile" element={<ProfilePage/>}/>
+        </Routes>
+      </section>
+    </div>
   )
 }
 
