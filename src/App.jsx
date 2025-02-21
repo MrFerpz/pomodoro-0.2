1b881cbbbb9f4ca68865af2582c03959b36d4402
Fred <leferpz@gmail.com>
2025-02-21 16:08:51 -0500

    Added two useEffects to set and access local storage for coins and stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import FightPage from './pages/FightPage/FightPage'
@@ -10,8 +10,32 @@ import SpinPage from "./pages/SpinPage/SpinPage"
 import { Provider } from "./components/ui/provider"
 
 function App() {
+  // initial state is empty
   const [coins, setCoins] = useState(0);
-  const [stats, setStats] = useState({completions: 0, sessionRecord: [], totalCoinsEarned: 0});
+  const [stats, setStats] = useState({completions: 0, sessionRecord: [], totaloinsEarned: 0});
+
+  // check if there is previous saved data
+  useEffect(() => {
+    const storedCoins = localStorage.getItem("coins");
+    const storedStats = localStorage.getItem("stats");
+
+    // if yes, set data to that
+    if (storedCoins) {
+      setCoins(parseInt(storedCoins, 10))
+      }
+
+    if (storedStats) {
+      setStats(JSON.parse(storedStats))
+    }
+    }, [])
+
+    // saving to storage every time stats or coins are updated
+    useEffect(() => {
+      localStorage.setItem("coins", coins);
+      localStorage.setItem("stats", JSON.stringify(stats))
+    },
+    [stats, coins]
+    )
 
   return (
     <Provider>
