2af259e6a513241243382e44f4bbfb5fb483ee92
Fred <leferpz@gmail.com>
2025-02-21 10:02:27 -0500

    Now runs a simple animation and the winning option is displayed at the end

--- src/pages/SpinPage/SpinPage.jsx
+++ src/pages/SpinPage/SpinPage.jsx
@@ -1,40 +1,32 @@
 import styles from "./SpinPage.module.css"
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Stack, Button } from "@chakra-ui/react"
 
 function SpinPage() {
     const [prizeList, setPrizeList] = [["10m break", "chess game", "walk", "croissant", "coffee", "early finish", "takeaway", "buy clothes"]]
     const [isSpinning, setIsSpinning] = useState(null)
-
-    // setting up a DOM reference so I can make CSS changes to the winning prize
-    const winningPrize = useRef(null)
+    const [selectedPrize, setSelectedPrize] = useState(null)
 
     function handleSpin() {
         let randomNumber = Math.round(Math.random()*(prizeList.length-1))
-        const selectedPrize = prizeList[randomNumber];
-        console.log(selectedPrize);
+        setSelectedPrize(prizeList[randomNumber]);
         setIsSpinning(true);
-
         // apply class after the duration of the animation
-        setTimeout(() => {}, 4000)
+        // setTimeout(() => {}, 4000)
     }
-
-    let normalBox = styles.box;
-    let animatedBox = styles.animatedBox;
 
     return (
         <div>
             <div className={styles.horizontalFlexContainer}>
             <Button onClick={handleSpin} colorPalette="teal" className={styles.spinButton}>Spin!</Button>
             <Stack>
-                <div className={isSpinning ? animatedBox : normalBox}>10m break</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Chess game</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Walk</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Croissant</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Coffee</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Early finish</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Takeaway</div>
-                <div className={isSpinning ? animatedBox : normalBox}>Buy clothes</div>
+                {prizeList.map((prize, index) => {
+                    return <div 
+                    className={`${styles.box} ${isSpinning ? styles.animatedBox : ''} ${prize === selectedPrize ? styles.selectedBox : ''}`}
+                    key={index}>
+                        {prize}
+                    </div>
+                })}
             </Stack>
             </div>
         </div>
