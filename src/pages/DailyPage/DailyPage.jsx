805a7eedb26b37a03df6e85bdee052dd7d0957b6
Fred <leferpz@gmail.com>
2025-07-06 15:45:40 +0100

    Added home button

--- src/pages/DailyPage/DailyPage.jsx
+++ src/pages/DailyPage/DailyPage.jsx
@@ -1,11 +1,13 @@
 import styles from "./DailyPage.module.css"
 import DailyTask from "../../components/DailyTask/DailyTask"
+import { Link } from "react-router"
+import Icons from "../../components/Icons"
 
 export default function DailyPage({coins, updateCoins}) {
     return (
         <div>
+            <div className={styles.flexContainer}><Link to="/"><div className={styles.icon}>{Icons.home}</div></Link></div>
             <div className={styles.title}>Daily Habits</div>
-            <div className={styles.coinsText}>You currently have {coins} coins</div>
             <div className={styles.dailyGrid}>
                 <DailyTask task={"Exercise"} updateCoins={updateCoins}/>
                 <DailyTask task={"Meditate"} updateCoins={updateCoins}/>
